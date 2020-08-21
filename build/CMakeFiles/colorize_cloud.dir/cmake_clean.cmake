65076a8e21c17f21143d75eef3dd6e4706c37f4f
okuboali <okanplusz@gmail.com>
2020-08-21 14:36:52 +0900

    first commit

---
+++ build/CMakeFiles/colorize_cloud.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "ui_pclviewer.h"
+  "moc_pclviewer.cpp"
+  "CMakeFiles/colorize_cloud.dir/main.cpp.o"
+  "CMakeFiles/colorize_cloud.dir/pclviewer.cpp.o"
+  "CMakeFiles/colorize_cloud.dir/moc_pclviewer.cpp.o"
+  "colorize_cloud.pdb"
+  "colorize_cloud"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/colorize_cloud.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
